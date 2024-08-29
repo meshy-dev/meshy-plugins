853c610f9ed2c6dc4eccb10c2751498a95179e0d
boliu <378677982@qq.com>
2024-08-29 16:33:17 +0800

    [Feature] Blender pulgin UI polish (#6)
    * refactor and polish
    
    * chore: Update version to 0.2.1

--- Blender/meshy-for-blender/MeshyPanels/TextToModelPanel.py
+++ Blender/meshy-for-blender/MeshyPanels/TextToModelPanel.py
@@ -1,7 +1,7 @@
 import json
 import bpy
 import tempfile
-from .Utils import GetApiKey
+from .Utils import get_api_key
 import requests
 import os
 
@@ -9,8 +9,8 @@ T2M_URL = "https://api.meshy.ai/v2/text-to-3d"
 taskList = []
 
 
-# Submit a task
-class SubmitTaskToRemote(bpy.types.Operator):
+# Submit task
+class SendSubmitRequest(bpy.types.Operator):
     bl_label = "Submit Task"
     bl_idname = "meshy.t2m_submit_task"
 
@@ -18,42 +18,36 @@ class SubmitTaskToRemote(bpy.types.Operator):
         if context.scene.t2m_prompt == "" or context.scene.t2m_prompt is None:
             self.report(type={"ERROR"}, message="Prompt cannot be empty!")
             return {"FINISHED"}
-        if context.scene.t2m_mode == "preview":
-            payload = {
-                "mode": "preview",
-                "prompt": context.scene.t2m_prompt,
-                "art_style": context.scene.t2m_art_style,
-                "negative_prompt": context.scene.t2m_negative_prompt,
-                "name": context.scene.t2m_task_name,
-            }
-            # with seed
-            if context.scene.t2m_seed != "":
-                payload["seed"] = int(context.scene.t2m_seed)
-            headers = {"Authorization": f"Bearer {GetApiKey()}"}
-            response = requests.post(
-                T2M_URL,
-                headers=headers,
-                json=payload,
-            )
-            response.raise_for_status()
-            self.report({"INFO"}, response.text)
-            return {"FINISHED"}
-        elif context.scene.t2m_mode == "refine":
-            payload = {
-                "mode": "refine",
-                "preview_task_id": context.scene.t2m_preview_task_id,
-            }
-            headers = {"Authorization": f"Bearer {GetApiKey()}"}
-            return {"FINISHED"}
+
+        # create preview task
+        payload = {
+            "mode": "preview",
+            "prompt": context.scene.t2m_prompt,
+            "art_style": context.scene.t2m_art_style,
+            "negative_prompt": context.scene.t2m_negative_prompt,
+            "name": context.scene.t2m_task_name,
+        }
+        # with seed
+        if context.scene.t2m_seed != "":
+            payload["seed"] = int(context.scene.t2m_seed)
+        headers = {"Authorization": f"Bearer {get_api_key()}"}
+        response = requests.post(
+            T2M_URL,
+            headers=headers,
+            json=payload,
+        )
+        response.raise_for_status()
+        self.report({"INFO"}, response.text)
+        return {"FINISHED"}
 
 
-# Refresh the task list
+# Refresh task list
 class RefreshTaskList(bpy.types.Operator):
     bl_label = "Refresh Task List"
     bl_idname = "meshy.t2m_refresh_task_list"
 
     def refreshOnePage(self, context):
-        headers = {"Authorization": f"Bearer {GetApiKey()}"}
+        headers = {"Authorization": f"Bearer {get_api_key()}"}
         response = requests.get(T2M_URL + "?sortBy=-created_at", headers=headers)
         # response = requests.get(T2M_URL, headers=headers)
         response.raise_for_status()
@@ -70,7 +64,7 @@ class RefreshTaskList(bpy.types.Operator):
 
 
 # Download the model
-class AcquireResultsFromRemote(bpy.types.Operator):
+class DownloadModel(bpy.types.Operator):
     bl_label = "Download Model"
     bl_idname = "t2m.download_model"
     downloadPath: bpy.props.StringProperty(name="download path", default="")
@@ -101,7 +95,7 @@ class RefineModel(bpy.types.Operator):
             "preview_task_id": self.modelId,
             "name": self.taskName,
         }
-        headers = {"Authorization": f"Bearer {GetApiKey()}"}
+        headers = {"Authorization": f"Bearer {get_api_key()}"}
         response = requests.post(
             T2M_URL,
             headers=headers,
@@ -112,14 +106,14 @@ class RefineModel(bpy.types.Operator):
         return {"FINISHED"}
 
 
-# Delete a task
+# Delete the task
 class DeleteTask(bpy.types.Operator):
     bl_label = "Delete Task"
     bl_idname = "t2m.delete_task"
     modelId: bpy.props.StringProperty(name="model id", default="")
 
     def execute(self, context):
-        headers = {"Authorization": f"Bearer {GetApiKey()}"}
+        headers = {"Authorization": f"Bearer {get_api_key()}"}
         response = requests.delete(
             T2M_URL + f"/{self.modelId}",
             headers=headers,
@@ -129,8 +123,7 @@ class DeleteTask(bpy.types.Operator):
         return {"FINISHED"}
 
 
-# Text to Model panel
-class TextToModelPanel(bpy.types.Panel):
+class MeshyTextToModel(bpy.types.Panel):
     bl_idname = "MESHY_PT_text_to_model"
     bl_label = "Text To Model"
     bl_space_type = "VIEW_3D"
@@ -140,90 +133,115 @@ class TextToModelPanel(bpy.types.Panel):
     # Draw the panel UI
     def draw(self, context):
         layout = self.layout
-        column = layout.column()
-        column.label(text="Mode:")
-        column.prop(context.scene, property="t2m_mode", text="")
-        if context.scene.t2m_mode == "preview":
-            column.label(text="Prompt:")
-            column.prop(context.scene, property="t2m_prompt", text="")
-            column.label(text="Negative Prompt:")
-            column.prop(context.scene, "t2m_negative_prompt", text="")
-            column.label(text="Task Name:")
-            column.prop(context.scene, "t2m_task_name", text="")
-            column.label(text="Art Style:")
-            column.prop(context.scene, "t2m_art_style", text="")
-            column.prop(context.scene, "t2m_seed", text="Seed")
-        elif context.scene.t2m_mode == "refine":
-            column.prop(context.scene, "t2m_preview_task_id", text="")
-        column.operator(SubmitTaskToRemote.bl_idname, text="Submit Task")
-        column.operator(RefreshTaskList.bl_idname, text="Refresh Task List")
+        # Display a collapsible box for task generation settings
+        col = layout.box().column(align=True)
+        row = col.row()
+        row.prop(
+            context.scene,
+            "t2m_expanded_task_settings",
+            icon=(
+                "TRIA_DOWN"
+                if context.scene.t2m_expanded_task_settings
+                else "TRIA_RIGHT"
+            ),
+            icon_only=True,
+            emboss=False,
+        )
+        row.label(text="Generation Settings")
+        if context.scene.t2m_expanded_task_settings:
+            col.label(text="Prompt:")
+            col.prop(context.scene, property="t2m_prompt", text="")
+            col.label(text="Negative Prompt:")
+            col.prop(context.scene, "t2m_negative_prompt", text="")
+            col.label(text="Task Name:")
+            col.prop(context.scene, "t2m_task_name", text="")
+            col.separator()
+            col.prop(context.scene, "t2m_art_style")
+            col.prop(context.scene, "t2m_seed", text="Seed")
 
-        if len(taskList) == 0:
-            return
+            col.separator()
 
-        def create_col(header: str, content_key: str):
-            col = split.column()
-            col.label(text=header)
+            # bigger button
+            row = col.row()
+            row.scale_y = 1.5
+            row.operator(SendSubmitRequest.bl_idname, text="Submit Task", icon="PLUS")
+
+        # Display a collapsible box for task list
+        col = layout.box().column(align=True)
+        row = col.row()
+        row.prop(
+            context.scene,
+            "t2m_expanded_task_list",
+            icon="TRIA_DOWN" if context.scene.t2m_expanded_task_list else "TRIA_RIGHT",
+            icon_only=True,
+            emboss=False,
+        )
+        row.label(text="Task List")
+        if context.scene.t2m_expanded_task_list:
+            col.operator(
+                RefreshTaskList.bl_idname, text="Refresh Task List", icon="FILE_REFRESH"
+            )
+
+            if len(taskList) == 0:
+                return
+
             for task in taskList:
+                col.separator()
                 if task["status"] in ["SUCCEEDED", "FAILED", "PENDING", "IN_PROGRESS"]:
-                    col.label(text=str(task[content_key]))
+                    row = col.row()
+                    row.label(text=task["name"])
+                    row.label(text=task["art_style"])
+                    row = col.row()
+                    row.label(text="Mode")
+                    row.label(text=task["mode"])
+                    row = col.row()
+                    row.label(text=f"Status {task['status']}")
+                    if task["status"] == "IN_PROGRESS":
+                        row.label(text=f"Progress {str(task['progress'])}")
 
-        split = layout.split()
-        col = split.column()
-        col.label(text="Download")
-        for task in taskList:
-            if task["status"] == "SUCCEEDED":
-                downloadButton = col.operator(
-                    AcquireResultsFromRemote.bl_idname, text="Download"
+                row = col.row()
+                if task["status"] == "SUCCEEDED":
+                    downloadButton = row.operator(
+                        DownloadModel.bl_idname, text="Download", icon="SORT_ASC"
+                    )
+                    downloadButton.downloadPath = task["model_urls"]["glb"]
+
+                if task["status"] == "SUCCEEDED" and task["mode"] != "refine":
+                    refineButton = row.operator(
+                        RefineModel.bl_idname, text="Refine", icon="IMAGE"
+                    )
+                    refineButton.modelId = task["id"]
+                    refineButton.taskName = task["name"]
+
+                deleteButton = row.operator(
+                    DeleteTask.bl_idname, text="Delete", icon="TRASH"
                 )
-                downloadButton.downloadPath = task["model_urls"]["glb"]
-            else:
-                col.label(text=" ")
-        create_col("Task Name", "name")
-        create_col("Mode", "mode")
-        create_col("Art Style", "art_style")
-        create_col("Status", "status")
-        create_col("Progress", "progress")
-        col = split.column()
-        col.label(text="Refine")
-        for task in taskList:
-            if task["status"] == "SUCCEEDED" and task["mode"] != "refine":
-                refineButton = col.operator(RefineModel.bl_idname, text="Refine")
-                refineButton.modelId = task["id"]
-                refineButton.taskName = task["name"]
-            else:
-                col.label(text=" ")
-        col = split.column()
-        col.label(text="Delete")
-        for task in taskList:
-            deleteButton = col.operator(DeleteTask.bl_idname, text="Delete")
-            deleteButton.modelId = task["id"]
+                deleteButton.modelId = task["id"]
 
 
-# Create properties shown in the panel
-def RegisterProperties():
-    bpy.types.Scene.t2m_mode = bpy.props.EnumProperty(
-        name="Mode",
-        description="Create a preview task, or refine task.",
-        items=[("preview", "Preview", ""), ("refine", "Refine", "")],
-    )
+# Create value we will use in all of the windows
+def CreateValue():
+    # The value we will use in text to texture
+    bpy.types.Scene.t2m_expanded_task_settings = bpy.props.BoolProperty(default=True)
+    bpy.types.Scene.t2m_expanded_task_list = bpy.props.BoolProperty(default=False)
     bpy.types.Scene.t2m_prompt = bpy.props.StringProperty(
         name="Prompt",
-        description="text_to_model_prompt",
+        description="Text to model prompt",
         default="",
     )
     bpy.types.Scene.t2m_art_style = bpy.props.EnumProperty(
         name="Art style",
-        description="text_to_model_art_style",
         items=[
             ("realistic", "Realistic", ""),
             ("cartoon", "Cartoon", ""),
             ("low-poly", "Low poly", ""),
         ],
+        description="Text to model art style",
+        default="realistic",
     )
     bpy.types.Scene.t2m_negative_prompt = bpy.props.StringProperty(
         name="Negative prompt",
-        description="text_to_model_negative_prompt",
+        description="Text to model negative prompt",
         default="",
     )
     bpy.types.Scene.t2m_seed = bpy.props.StringProperty(
@@ -233,24 +251,26 @@ def RegisterProperties():
     )
     bpy.types.Scene.t2m_task_name = bpy.props.StringProperty(
         name="Task name",
-        description="text_to_model_task_name",
+        description="Text to model task name",
         default="Meshy_model",
     )
 
 
-def UnRegisterProperties():
-    del bpy.types.Scene.t2m_mode
+# Delete the value we have created
+def DeleteValue():
     del bpy.types.Scene.t2m_prompt
     del bpy.types.Scene.t2m_art_style
     del bpy.types.Scene.t2m_negative_prompt
     del bpy.types.Scene.t2m_task_name
+    del bpy.types.Scene.t2m_expanded_task_settings
+    del bpy.types.Scene.t2m_expanded_task_list
 
 
 classes = (
-    TextToModelPanel,
-    SubmitTaskToRemote,
+    MeshyTextToModel,
+    SendSubmitRequest,
     RefreshTaskList,
-    AcquireResultsFromRemote,
+    DownloadModel,
     RefineModel,
     DeleteTask,
 )
@@ -259,10 +279,10 @@ classes = (
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
-    RegisterProperties()
+    CreateValue()
 
 
 def unregister():
-    UnRegisterProperties()
+    DeleteValue()
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
--- Blender/meshy-for-blender/MeshyPanels/Utils.py
+++ Blender/meshy-for-blender/MeshyPanels/Utils.py
@@ -1,7 +1,8 @@
 import bpy
 
 
-def GetApiKey():
+# Get local api key
+def get_api_key():
     user_preferences = bpy.context.preferences
     addon_preferences = user_preferences.addons["meshy-for-blender"].preferences
     return addon_preferences.api_key
